cdac83ffdf5378d72b37542a23bfdff98b2a8c4f
takov751 <40316768+takov751@users.noreply.github.com>
2023-04-05 04:19:55 +0100

    adding kobo glo hd screen size

--- src/display/sizing.rs
+++ src/display/sizing.rs
@@ -1,3 +1,4 @@
 use crate::coords::PixelSpaceCoord;
 
 pub const KOBO_AURA_SCREEN_SIZE_PX: PixelSpaceCoord = PixelSpaceCoord::new(740, 1000);
+pub const KOBO_GLO_HD_SCREEN_SIZE_PX: PixelSpaceCoord = PixelSpaceCoord::new(1072, 1448);
